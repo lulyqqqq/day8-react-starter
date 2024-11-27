8906c66646b025274f83a52be3b70bf853cc5d51
Mason ma <MAMA14@oocl.com>
2024-11-27 15:46:03 +0800

    feat:parent add css

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,11 +3,13 @@ import "./ProductionItem.scss"
 const ProductItem = (props) => {
     const {productItem} = props
     return (
-        <div className={"product-item-wrapper"}>
-            <span>{productItem.name}</span>
-            <span>{productItem.price}</span>
-            {productItem.status ? <span className={"product-status-greed"}>In Stock</span> :
-                <span className={"product-status-red"}>Out Stock</span>}
+        <div className={"product-item-wrapper-div"}>
+            <div className={"product-item-wrapper"}>
+                <span>{productItem.name}</span>
+                <span>{productItem.price}</span>
+                {productItem.status ? <span className={"product-status-greed"}>In Stock</span> :
+                    <span className={"product-status-red"}>Out Stock</span>}
+            </div>
         </div>
     )
 }
